debc99db7a550c542718199b2c2b5c68aafea5fa
Sergio Rodriguez <sergio.rolo94@gmail.com>
2021-10-01 17:22:14 +0200

    Iteración 1

---
+++ Iteration1.js
@@ -0,0 +1,11 @@
+let myFavoriteHero = "Hulk";
+let x = 50;
+let h = 5;
+let y = 10;
+let z = h + y;
+
+console.log(myFavoriteHero);
+console.log(x);
+console.log(h);
+console.log(y);
+console.log(z);
