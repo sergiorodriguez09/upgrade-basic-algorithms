baf42d61b5267ef7eca4cfe2ff490a14df16df54
Sergio Rodriguez <sergio.rolo94@gmail.com>
2021-10-01 20:00:35 +0200

    Iteración 2, ejercicio estaba sin finalizar

--- Iteration2.js
+++ Iteration2.js
@@ -8,3 +8,19 @@ let lastName = "Snow";
 let age = 24;
 
 console.log("Soy" + " " + firstName + " " + lastName + " " + ", tengo" + " " + age + " " + "años y me gustan los lobos.");
+
+const toy1 = {name: "Buss myYear" , price: 19};
+const toy2 = {name: "Rallo mcKing" , price: 20};
+const totalPrice = toy1.price + toy2.price;
+
+console.log(totalPrice);
+
+let globalBasePrice = 10000;
+globalBasePrice = 25.000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+car1.finalPrice = car1.basePrice + globalBasePrice;
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+car2.finalPrice = car2.basePrice + globalBasePrice;
+
+console.log(car1.finalPrice);
+console.log(car2.finalPrice);
