b0e67100a83ddd8efafa818b37530f6e87cebd7b
Sergio Rodriguez <sergio.rolo94@gmail.com>
2021-10-01 21:11:29 +0200

    Iteracion 4

---
+++ Iteration4.js
@@ -0,0 +1,26 @@
+/*const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+const avenger = avengers [0];
+console.log(avenger);*/
+
+/*const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+avengers.shift("HULK");
+avengers.unshift("IRONMAN");
+console.log(avengers);*/
+
+/*const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers.length);*/
+
+/*const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty" , "Summer");
+console.log(rickAndMortyCharacters [4]);*/
+
+
+/*const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters.pop();
+console.log(rickAndMortyCharacters);
+console.log( rickAndMortyCharacters [0]);
+console.log( rickAndMortyCharacters [4]);*/
+
+/*const rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters.shift("Beth");
+console.log(rickAndMortyCharacters);*/
