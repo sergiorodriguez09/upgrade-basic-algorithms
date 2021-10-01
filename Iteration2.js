ae9dc3900ed7d9bfe01ccaf46e7477299a4eed9d
Sergio Rodriguez <sergio.rolo94@gmail.com>
2021-10-01 17:54:52 +0200

    Iteración 2

---
+++ Iteration2.js
@@ -0,0 +1,10 @@
+const character = {name: 'Jack Sparrow', age: 10};
+character.age = 25;
+
+console.log(character);
+
+let firstName = "Jon";
+let lastName = "Snow";
+let age = 24;
+
+console.log("Soy" + " " + firstName + " " + lastName + " " + ", tengo" + " " + age + " " + "años y me gustan los lobos.");
